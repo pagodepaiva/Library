c7118563634758010a8a447af2b02e2be4103096
pagodepaiva <90423475+pagodepaiva@users.noreply.github.com>
2024-06-14 11:15:28 -0300

    Delete Codes/Strings/kmp.cpp

--- Codes/Strings/kmp.cpp
+++
@@ -1,8 +0,0 @@
-for(int i = 1;i < kmp.size();i++){
-        int j = p[i-1];
-        while(j > 0 and kmp[i] != kmp[j])
-            j = p[j-1];
-        if(kmp[i] == kmp[j])
-            j++;
-        p[i] = j;
-    }
